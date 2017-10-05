7380ca7ac6ddaa7cdbd1bd2c5a39d83fc7e5636b
Suren Khorenyan <surenkhorenyan@gmail.com>
2017-10-05 21:29:25 +0300

    Initial Commit

---
+++ TinyLogger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TinyLogger
+//
+//  Created by Suren Khorenyan on 05.10.17.
+//  Copyright © 2017 Suren Khorenyan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
